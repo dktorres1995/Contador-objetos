cb4d55e8aba98807abce87f4733cffefbaab772f
dktorres1995 <59892053+dktorres1995@users.noreply.github.com>
2020-04-29 12:22:28 -0500

    Update appcenter-post-clone.sh

--- tomarfoto/android/app/appcenter-post-clone.sh
+++ tomarfoto/android/app/appcenter-post-clone.sh
@@ -5,17 +5,20 @@ cd ..
 set -e
 # debug log
 set -x
-
+ 
 cd ..
 # choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
 # stable - recommended for production
-
+ 
 git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
-
+ 
 flutter channel stable
+flutter upgrade --force
+flutter pub cache repair
 flutter doctor
 flutter build apk --release
-
+ 
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_<
+ 
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
